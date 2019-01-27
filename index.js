3db33bd2bd57ed410c7fe38474bbbbe57042af40
Olivia Silva <olivia.silva@wework.com>
2019-01-27 00:47:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`);
   return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
+
+
+function nowServing(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.`);
+}
